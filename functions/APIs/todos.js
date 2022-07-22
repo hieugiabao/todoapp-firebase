21c1343c0ee2e3f5e485e73a19a1631ee677a50d
hieugiabao <hieugiabao12@gmail.com>
2022-07-22 10:42:28 +0700

    todo CRUD

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -21,3 +21,65 @@ exports.getAllTodos = (request, response) => {
       return response.status(500).json({ error: err.code });
     });
 };
+
+exports.postOneTodo = (request, response) => {
+  if (request.body.body.trim() === "") {
+    return response.status(400).json({ body: "Can not be empty" });
+  }
+  if (request.body.title.trim() === "") {
+    return response.status(400).json({ title: "Can not be empty" });
+  }
+
+  const newItem = {
+    title: request.body.title,
+    body: request.body.body,
+    createdAt: new Date().toISOString(),
+  };
+
+  db.collection("todos")
+    .add(newItem)
+    .then((doc) => {
+      return response.json({
+        id: doc.id,
+        ...newItem,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: "Something went wrong" });
+    });
+};
+
+exports.deleteTodo = (request, response) => {
+  const document = db.doc(`/todos/${request.params.todoId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return response.status(404).json({ error: "Todo not found" });
+      }
+      return document.delete();
+    })
+    .then(() => {
+      return response.json({ message: "Todo deleted successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
+};
+
+exports.editTodo = (request, response) => {
+  if (request.body.todoId || request.body.createdAt) {
+    response.status(403).json({ error: "Not allow to edit Todo" });
+  }
+
+  let document = db.collection("/todos").doc(`${request.params.todoId}`);
+  document
+    .update(request.body)
+    .then(() => response.json({ message: "Update successfully" }))
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
+};
